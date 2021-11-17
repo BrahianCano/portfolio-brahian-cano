0ca2fbd981a24205125d4bc187a1dcd4a4c417ae
brahian <brahian.stiben@talent.com>
2021-11-16 20:00:44 -0500

    Fix: Change entry point vite second try

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'index.html',
-      formats: ['es']
+      entry: "src/my-element.ts",
+      formats: ["es"],
     },
     rollupOptions: {
-      external: /^lit/
-    }
-  }
-})
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
