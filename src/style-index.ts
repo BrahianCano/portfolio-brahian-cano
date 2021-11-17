36dd25ae41bb2f70ad607be58b558ac98f44ca5f
brahian <brahian.stiben@talent.com>
2021-11-16 19:06:17 -0500

    Feat: Create new app distribution

---
+++ src/style-index.ts
@@ -0,0 +1,10 @@
+import { css } from "lit";
+
+export const styleGlobal = css`
+:host {
+    font-family: 'Poppins', sans-serif;
+    display: block;
+    width: 100%;
+    -webkit-font-smoothing: antialiased;
+}
+`
